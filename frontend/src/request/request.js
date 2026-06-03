49375f31e3544d76305380c4ecc6446a859d46e1
JC <576461713@qq.com>
2026-06-03 11:15:07 +0800

    fix: use hash router history

---
+++ frontend/src/request/request.js
@@ -0,0 +1,114 @@
+import axios from 'axios';
+
+import { isUndefined } from '@/utils';
+import { STATUS_CODE, X_WA_TOKEN } from './constant';
+import { setCommonHeader } from './processor';
+import { useParamsStore } from '@/store';
+
+const instance = axios.create({
+  baseURL: '',
+  timeout: 6000000,
+  withCredentials: false,
+  transformResponse: [
+    (data) => {
+      if (isUndefined(data) || typeof data !== 'string') {
+        return data;
+      }
+
+      try {
+        try {
+          const normalizedData = data.replace(/('|")id('|")\: *[0-9]{16,}/g, (bigint) => {
+            return bigint.replace(/[0-9]{16,}/g, (big) => {
+              return `"${big}"`;
+            });
+          });
+          return JSON.parse(normalizedData);
+        } catch (error) {
+          return JSON.parse(data);
+        }
+      } catch (error) {
+        return data;
+      }
+    }
+  ]
+});
+
+const errorProcess = {
+  [STATUS_CODE.UNAUTHORIZED]: (response = {}) => {
+    if (isUndefined(response.data?.data?.loginUrl)) {
+      return;
+    }
+
+    window.location = `${response.data?.data?.loginUrl}${encodeURIComponent(window.location.href)}`;
+  }
+};
+
+const preProcess = (headers) => {
+  setCommonHeader(headers);
+};
+
+instance.interceptors.request.use(
+  (config) => {
+    preProcess(config.headers);
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    if (response.headers[X_WA_TOKEN]) {
+      const store = useParamsStore();
+      store.setToken(response.headers[X_WA_TOKEN]);
+    }
+
+    if (response.data?.success === false) {
+      return Promise.reject(response.data);
+    }
+
+    return response.data?.data ?? response.data;
+  },
+  (error) => {
+    const { response } = error;
+    const process = errorProcess[response?.status];
+
+    if (!isUndefined(process)) {
+      process(response);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export const axiosInstance = (method = 'get', url = '', params = {}, data = {}, config = {}) => {
+  return instance({ method, url, params, data, ...config });
+};
+
+export const uploadInstance = (url = '', file, data = {}, config = {}) => {
+  const formData = new FormData();
+  formData.append('file', file);
+
+  Object.entries(data).forEach(([key, value]) => {
+    if (!isUndefined(value) && value !== null) {
+      formData.append(key, value);
+    }
+  });
+
+  return axiosInstance('post', url, {}, formData, {
+    ...config,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      ...config.headers
+    }
+  });
+};
+
+export const request = {
+  get: (url, params = {}, config = {}) => axiosInstance('get', url, params, {}, config),
+  post: (url, data = {}, config = {}) => axiosInstance('post', url, {}, data, config),
+  put: (url, data = {}, config = {}) => axiosInstance('put', url, {}, data, config),
+  delete: (url, params = {}, config = {}) => axiosInstance('delete', url, params, {}, config),
+  upload: uploadInstance
+};
+
+export default request;
---
+++ frontend/src/store/userStore.js
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia';
+
+export const useUserStore = defineStore('user', {
+  state() {
+    return {
+      _id: '',
+      _avatar: '', // 头像
+      _employeeNumber: '',
+      _name: '',
+      _role: ''
+    };
+  },
+
+  getters: {
+    id: (state) => state._id,
+    avatar: (state) => state._avatar,
+    name: (state) => state._name,
+    employeeNumber: (state) => state._employeeNumber,
+    role: (state) => state._role
+  },
+
+  actions: {
+    setUserInfo(userInfo = {}) {
+      const { id, _id, name, avatar, employeeNumber, role } = userInfo;
+      this._id = id || _id || '';
+      this._name = name || '';
+      this._employeeNumber = employeeNumber || '';
+      this._avatar = avatar || '';
+      this._role = role || '';
+    }
+  }
+});
