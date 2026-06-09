cf74a499d225d4b87986656761cfba2eee18dbab
JC <576461713@qq.com>
2026-06-09 09:17:19 +0800

    Use hot icon for skill usage

--- frontend/src/components/SkillHub.vue
+++ frontend/src/components/SkillHub.vue
@@ -18,7 +18,7 @@
         <div class="skill-card__footer">
           <div class="skill-card__meta">
             <span class="skill-card__usage">
-              <span class="skill-card__usage-icon" aria-hidden="true">♨</span>
+              <img class="skill-card__usage-icon" :src="hotIcon" alt="" aria-hidden="true" />
               {{ formatUsageCount(getSkillUsageCount(skill)) }} 次使用
             </span>
             <span v-if="getSkillAuthorText(skill)" class="skill-card__author" :title="getSkillAuthorText(skill)">
@@ -143,6 +143,8 @@ import { useUserStore } from '@/store';
 import downloadHighIcon from '@/assets/download_high.svg';
 import downloadNormalIcon from '@/assets/download_normal.svg';
 import moreIcon from '@/assets/more.svg';
+
+const hotIcon = 'assets/hot.svg';
 
 const userStore = useUserStore();
 const skills = ref([]);
